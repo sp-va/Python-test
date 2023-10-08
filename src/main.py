2083454b64e0943302c8d1e663667510e3c4fcd8
spirin-vadim <vadis99@ya.ru>
2023-10-08 11:21:54 +0500

    changed request method for '/create-city/' endpoint

--- src/main.py
+++ src/main.py
@@ -10,20 +10,19 @@ from src.schemas import *
 app = FastAPI()
 
 
-@app.get('/create-city/', summary='Create City', description='Создание города по его названию')
-def create_city(city: str = Query(description="Название города", default=None)):
+@app.post('/create-city/', summary='Create City', description='Создание города по его названию')
+def create_city(city: str = Query(description="Название города", default=None), session: Session = Depends(get_session)):
     if city is None:
         raise HTTPException(status_code=400, detail='Параметр city должен быть указан')
     check = CheckCityExisting()
     if not check.check_existing(city):
         raise HTTPException(status_code=400, detail='Параметр city должен быть существующим городом')
 
-    city_object = Session().query(City).filter(City.name == city.capitalize()).first()
+    city_object = session.query(City).filter(City.name == city.capitalize()).first()
     if city_object is None:
         city_object = City(name=city.capitalize())
-        s = Session()
-        s.add(city_object)
-        s.commit()
+        session.add(city_object)
+        session.commit()
 
     return {'id': city_object.id, 'name': city_object.name, 'weather': city_object.weather}
 
